e8d50b43673299d335d43a2709721df322c01d65
viet.phan <viet.phan@tiki.vn>
2021-04-14 17:12:09 +0700

    fix port

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/videos', (req, response) => {
   })
 })
 
-const server = app.listen(5000, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
   const host = server.address().address
   const port = server.address().port
 
